b472049e50afcae1995fac3fc1c3513b2f20176f
Amit Singh <amisingh@guidewire.com>
2022-10-07 17:29:37 +0530

    comments out change
    Signed-off-by: Kanchan Dhamane <kdhamane@guidewire.com>

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,7 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-		System.out.println(System.getenv("CLASSPATH"));
+		//System.out.println(System.getenv("CLASSPATH"));
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
